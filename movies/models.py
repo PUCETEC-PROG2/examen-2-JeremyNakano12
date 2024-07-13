1f10aa8d6e0c229a27a46018bf9b4d40b91d91b0
Jeremy Marin <jmarin@puce.edu.ec>
2024-07-13 09:50:43 -0500

    Programa terminado

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Movies (models.Model):
+    title = models.CharField(max_length=30, null=False)
+    genre = models.CharField(max_length=15, null=False)
+    director = models.CharField(max_length=30, null=False)
+    release_year = models.CharField(max_length=12, null=False)
+    sinopsis = models.TextField(max_length=300, null=False)
+    
+    def __str__(self) -> str:
+        return self.title
--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Movies
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    movie = Movies.objects.order_by('genre')
+    template = loader.get_template('index.html')
+    return HttpResponse(template.render({'movie': movie}, request))
+
+def movie(request, movie_id):
+    movie = Movies.objects.get(pk = movie_id)
+    template = loader.get_template('display_movie.html')
+    context = {
+        'movie': movie
+    }
+    return HttpResponse(template.render(context, request))
